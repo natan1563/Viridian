6c8dcb731c8d647450d6cd616cc5f1f51ecf15cf
natan1563 <natromafe@gmail.com>
2023-04-01 19:46:02 -0300

    finished application

--- Atividade3/V-Nata/atividade-ajax-fipe/js/app.js
+++ Atividade3/V-Nata/atividade-ajax-fipe/js/app.js
@@ -1,22 +1,26 @@
-const baseUrl = "https://parallelum.com.br/fipe/api/v1"
-const endpointMarcas = `${baseUrl}/carros/marcas`
-const endpointModels = `${baseUrl}/carros/marcas/{MARCA_ID}/modelos`
-const endpointAnos   = `${baseUrl}/carros/marcas/{MARCA_ID}/modelos/{MODEL_ID}/anos`
+let apiUrl         = "https://parallelum.com.br/fipe/api/v1/{VEHICLE}"
+let endpointMarcas = endpointModels = endpointAnos = endpointValoresModal = ''
 
+const listaDeVeilos  = document.getElementById('vehicles_types')
 const listaDeMarcas  = document.getElementById("vehicles_brand")
 const listaDeModelos = document.getElementById("vehicles_model")
 const listaDeAnos    = document.getElementById("vehicles_year")
 
-realizarRequisicao(endpointMarcas)
-.then(response => {
-  response.forEach(marca => {
-    const novoItem = document.createElement("option")
-    novoItem.value = marca.codigo
-    novoItem.innerText = marca.nome
-    listaDeMarcas.appendChild(novoItem)
-  })
+const botaoDeBusca   = document.querySelector('.search_button')
+const modalPreco     = document.querySelector('.modal')
+const botaoFechar    = modalPreco.querySelector('.close')
 
-  listaDeMarcas.removeAttribute("disabled")
+const defaultOption  = '<option disabled selected></option>'
+
+listaDeVeilos.addEventListener("click", (event) => {
+  const tiposPermitidos = ['carros', 'motos', 'caminhoes']
+  const tipoVeiculoAtual = event.target.dataset.type || event.target.parentElement.dataset.type
+
+  if (!tiposPermitidos.includes(tipoVeiculoAtual)) return
+
+  atualizarTipoDoVeiculo(tipoVeiculoAtual, event.target)
+  buscarPorTipoVeiculo(tipoVeiculoAtual)
+  visibilidadeDoBotao()
 })
 
 listaDeMarcas.addEventListener("change", (event) => {
@@ -39,18 +43,15 @@ listaDeMarcas.addEventListener("change", (event) => {
 })
 
 listaDeModelos.addEventListener("change", (event) => {
-  const opcaoAtual = event.target.value
-  if (!opcaoAtual) return 
+  const modelId = event.target.value
+  if (!modelId) return 
 
-  const modelId = listaDeMarcas.options[listaDeMarcas.selectedIndex].value
-  debugger
-  let endpoint = endpointAnos.replace("{MARCA_ID}", opcaoAtual)
+  const marcaId = listaDeMarcas.options[listaDeMarcas.selectedIndex].value
+  let endpoint = endpointAnos.replace("{MARCA_ID}", marcaId)
   endpoint = endpoint.replace("{MODEL_ID}", modelId)
 
-  console.log(endpoint)
   realizarRequisicao(endpoint)
   .then(response => {
-    console.log(response)
     response.forEach(ano => {
       const novoAno = document.createElement("option")
       novoAno.value = ano.codigo
@@ -62,6 +63,116 @@ listaDeModelos.addEventListener("change", (event) => {
   })
 })
 
+listaDeAnos.addEventListener('change', visibilidadeDoBotao)
+
+botaoDeBusca.addEventListener('click', () => {
+  const marcaId = listaDeMarcas.options[listaDeMarcas.selectedIndex].value
+  const modelId = listaDeModelos.options[listaDeModelos.selectedIndex].value
+  const anoId   = listaDeAnos.options[listaDeAnos.selectedIndex].value
+  const testYear = /([0-9]{4})-([0-9]{1})/
+
+  if (!(isNumber(marcaId) && isNumber(modelId) && anoId.length && testYear.test(anoId)))
+    return 
+
+  limparDadosModal()
+
+  let endpoint = endpointValoresModal.replace("{MARCA_ID}", marcaId)
+  endpoint = endpoint.replace("{MODEL_ID}", modelId)
+  endpoint = endpoint.replace("{YEAR_ID}", anoId)
+
+  realizarRequisicao(endpoint)
+  .then(response => {
+    modalPreco.querySelector('.vehicle_name').innerText = response.Modelo
+    modalPreco.querySelector('.price').innerText = response.Valor
+
+    modalPreco.querySelector('.reference_month .value').innerText = response.MesReferencia
+    modalPreco.querySelector('.fipe_code .value').innerText = response.CodigoFipe
+    modalPreco.querySelector('.brand .value').innerText = response.Marca
+    modalPreco.querySelector('.year .value').innerText = response.AnoModelo
+
+    modalPreco.classList.remove('hide_modal')
+  })
+
+})
+
+botaoFechar.addEventListener('click', () => {
+  modalPreco.classList.add('hide_modal')
+})
+
 function realizarRequisicao(endpoint) {
   return fetch(endpoint).then(response => response.json())
+}
+
+function buscarPorTipoVeiculo() {
+  realizarRequisicao(endpointMarcas)
+  .then(response => {
+    listaDeMarcas.innerHTML = defaultOption
+    response.forEach(marca => {
+      const novoItem = document.createElement("option")
+      novoItem.value = marca.codigo
+      novoItem.innerText = marca.nome
+      listaDeMarcas.appendChild(novoItem)
+    })
+
+    listaDeMarcas.removeAttribute("disabled")
+  })
+}
+
+function atualizarTipoDoVeiculo(type, target) {
+  apiUrl = apiUrl.replace(/{VEHICLE}|carros|motos|caminhoes/, type)
+  endpointMarcas = `${apiUrl}/marcas`
+  endpointModels = `${endpointMarcas}/{MARCA_ID}/modelos`
+  endpointAnos   = `${endpointModels}/{MODEL_ID}/anos`
+  endpointValoresModal = `${endpointAnos}/{YEAR_ID}`
+
+  atualizarVeiculoAtivo(target)
+}
+
+function atualizarVeiculoAtivo(target) {
+  document.querySelector('[data-type="caminhoes"]').classList.remove('active')
+  document.querySelector('[data-type="carros"]').classList.remove('active')
+  document.querySelector('[data-type="motos"]').classList.remove('active')
+  
+  reiniciarTodosOsCampos()
+
+  if (target.dataset.type) {
+    target.classList.add('active')
+    return
+  }
+
+  target.parentElement.classList.add('active')
+}
+
+function reiniciarTodosOsCampos() {
+  listaDeMarcas.setAttribute('disabled', true)
+  listaDeModelos.setAttribute('disabled', true)
+  listaDeAnos.setAttribute('disabled', true)
+
+  listaDeMarcas.innerHTML = defaultOption
+  listaDeModelos.innerHTML = defaultOption
+  listaDeAnos.innerHTML = defaultOption
+}
+
+function visibilidadeDoBotao() {
+  if (listaDeAnos.length <= 1 && listaDeMarcas.length <= 1 && listaDeModelos.length <= 1) {
+    botaoDeBusca.style.cursor = 'default'
+    botaoDeBusca.style.opacity = 0.2
+    return
+  }
+
+  botaoDeBusca.style.cursor = 'pointer'
+  botaoDeBusca.style.opacity = 100
+}
+
+function limparDadosModal() {
+  modalPreco.querySelector('.vehicle_name').innerText = ''
+  modalPreco.querySelector('.price').innerText = ''
+  modalPreco.querySelector('.reference_month .value').innerText = ''
+  modalPreco.querySelector('.fipe_code .value').innerText = ''
+  modalPreco.querySelector('.brand .value').innerText = ''
+  modalPreco.querySelector('.year .value').innerText = ''
+}
+
+function isNumber(variable) {
+  return !isNaN(variable)
 }
